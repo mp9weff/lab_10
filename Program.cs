6d6d5144f48e5750bb5be5762b3c8ed1b415539f
unknown <mariapusahuk4@gmail.com>
2023-05-10 18:32:56 +0300

    lab_10

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створення екземпляра класу Person
+            Person person = new Person("John", new DateTime(1990, 1, 1));
+
+            // Створення екземпляра BinaryFormatter
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            // Серіалізуємо об'єкт у файл
+            using (FileStream stream = new FileStream("person.bin", FileMode.Create))
+            {
+                formatter.Serialize(stream, person);
+            }
+
+            // Десеріалізація об'єкта з файлу
+            using (FileStream stream1 = new FileStream("person.bin", FileMode.Open))
+            {
+                Person deserializedPerson = (Person)formatter.Deserialize(stream1);
+                Console.WriteLine(deserializedPerson.ToString());
+            }
+        }
+    }
+}
